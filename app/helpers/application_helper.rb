9058a35deb6d9cf71550134f0054c4cfd5cbed9e
Eugene <eugenecheonghy@gmail.com>
2016-04-20 13:20:06 +0000

    new stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     text.upcase
   end
 
+  def country_name(country_code)
+    country = ISO3166::Country[country_code]
+    country.translations[I18n.locale.to_s]
+  end
+
 end
